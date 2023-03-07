7841153cfa0b9786828bdf4029c4ea995bfc153f
Adrian Cruz <adr.ca@live.com>
2023-03-07 18:50:29 -0500

    tests

---
+++ templates/react-vite/src/App.test.tsx
@@ -0,0 +1,13 @@
+import { describe, test, expect } from 'vitest'
+import { render, screen } from '@testing-library/react'
+import App from './App'
+
+describe('<App />', () => {
+  test('App mounts properly', async () => {
+    const wrapper = render(<App />)
+    expect(wrapper).toBeTruthy()
+
+    // See the docs for writing tests
+    // https://testing-library.com/docs/react-testing-library/intro/
+  })
+})
