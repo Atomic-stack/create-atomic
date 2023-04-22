e7c98904b8edf44241d2981ec76accbf164ba75a
Adrian Cruz <adr.ca@live.com>
2023-04-22 15:54:05 -0400

    update scripts

--- templates/next-ts-app/next.config.js
+++ templates/next-ts-app/next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    // The regexes defined here are processed in Rust so the syntax is different from
+    // JavaScript `RegExp`s. See https://docs.rs/regex.
+    reactRemoveProperties: { properties: ['^data-test$'] },
+    removeConsole: {
+      exclude: ['error'],
+    },
+  },
   experimental: {
     appDir: true,
   },
